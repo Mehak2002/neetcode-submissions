3293a2d29b4ef3a4283a5309726934ada21a7f22
MEHAKPREET KAUR SAINI <85110764+Mehak2002@users.noreply.github.com>
2026-05-03 17:19:20 +0530

    Add: insertionSort - submission-1

---
+++ Data Structures & Algorithms/insertionSort/submission-1.java
@@ -0,0 +1,29 @@
+// Definition for a pair
+// class Pair {
+//     int key;
+//     String value;
+//
+//     Pair(int key, String value) {
+//         this.key = key;
+//         this.value = value;
+//     }
+// }
+public class Solution {
+    public List<List<Pair>> insertionSort(List<Pair> pairs) {
+   
+     List<List<Pair>> steps=new ArrayList<>();
+        if(pairs==null || pairs.size()==0) return steps;
+     steps.add(new ArrayList<>(pairs));
+    for(int i=1;i<pairs.size();i++){
+        Pair current=pairs.get(i);
+        int j=i-1;
+        while(j>=0 && pairs.get(j).key>current.key){
+            pairs.set(j+1,pairs.get(j));
+            j--;
+        }
+        pairs.set(j+1,current);
+        steps.add(new ArrayList<>(pairs));
+    }
+    return steps;
+    }
+}
