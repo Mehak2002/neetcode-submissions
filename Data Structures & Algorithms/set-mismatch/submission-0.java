7dcc465fb402d0ab44049659c428ff749a6fce4c
MEHAKPREET KAUR SAINI <85110764+Mehak2002@users.noreply.github.com>
2026-05-19 03:51:06 +0530

    Add: set-mismatch - submission-0

---
+++ Data Structures & Algorithms/set-mismatch/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int[] ans=new int[2];
+        for(int i=0;i<nums.length;i++){
+            int index=Math.abs(nums[i])-1;
+            if(nums[index]<0){
+                ans[0]=Math.abs(nums[i]);
+            }
+            else{
+                nums[index]*=-1;
+            }
+        }
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0){
+                ans[1]=i+1;
+
+            }
+        }
+        return ans;
+    }
+}
